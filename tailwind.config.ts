73fafcde068b173192437cd23fe55485306f8a50
Usman Ali Qureshi <usman@usmanaliqureshi.com>
2023-08-18 11:33:22 +0500

    Fixed issues with webkit rendering

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,7 @@ const config: Config = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    'node_modules/preline/dist/*.js',
+    './node_modules/preline/dist/*.js',
   ],
   theme: {
     extend: {
@@ -17,6 +17,7 @@ const config: Config = {
     },
   },
   plugins: [
+    require('@tailwindcss/typography'),
     require('preline/plugin'),
   ],
 }
