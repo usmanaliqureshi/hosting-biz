f8dfa3203cdeb93e5a3d87abf228d03dff0f34b5
Usman Ali Qureshi <usman@usmanaliqureshi.com>
2024-05-26 16:30:43 +0500

    Made cancle button functional

--- src/app/ui/add-customer.tsx
+++ src/app/ui/add-customer.tsx
@@ -1,11 +1,13 @@
 "use server"
 
+import { redirect } from "next/dist/server/api-utils";
 import { addCustomer } from "../lib/actions";
+import { revalidatePath } from "next/cache";
 
 export default async function AddCustomerForm() {
 
     return (
-        <form action={addCustomer(formData: FormData)}>
+        <form action={addCustomer}>
         {/* Section */}
         <div className="py-6 first:pt-0 last:pb-0 border-t first:border-transparent border-gray-200 dark:border-neutral-700 dark:first:border-transparent">
           <label
@@ -35,12 +37,11 @@ export default async function AddCustomerForm() {
               placeholder="Phone Number"
             />
                   <div className="mt-5 flex justify-end gap-x-2">
-                    <button
-                      type="button"
-                      className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800"
+                    <a className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800"
+                    href="/customers"
                     >
                       Cancel
-                    </button>
+                    </a>
                     <button
                       type="button"
                       className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
