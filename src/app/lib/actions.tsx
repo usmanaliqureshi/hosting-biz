d8d6abd74de66aaf628ee834446f1446eb27be12
Usman Ali Qureshi <usman@usmanaliqureshi.com>
2024-05-27 18:47:49 +0500

    Improved add customer data

--- src/app/lib/actions.tsx
+++ src/app/lib/actions.tsx
@@ -3,6 +3,7 @@
 import { z } from 'zod';
 import { sql } from "@vercel/postgres";
 import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 
 export async function deleteCustomer(id: string) {
     try {
@@ -18,37 +19,39 @@ const FormSchema = z.object({
     fullname: z.string(),
     email: z.string(),
     phone: z.coerce.number(),
-    status: z.enum(['Active', 'In-Active']),
+    status: z.string(),
 });
 
-const CreateCustomer = FormSchema.omit({ status: true });
+export async function addCustomer(data: FormData) {
 
-export async function addCustomer(formData: FormData) {
+    const rawFormData = Object.fromEntries(data.entries())
 
-    const { fullname, email, phone } = CreateCustomer.parse({
-        fullname: formData.get('name'),
-        email: formData.get('email'),
-        phone: formData.get('phone'),
+    const { fullname, email, phone } = FormSchema.parse({
+        fullname: rawFormData.name,
+        email: rawFormData.email,
+        phone: rawFormData.phone,
         status: 'Active',
     });
 
-    const rawFormData = Object.fromEntries(formData.entries())
+    if (typeof fullname === 'undefined' || fullname === '') {
+        throw new Error('Please provide a valid Full name');
+    } else if (typeof email === 'undefined' || email === '') {
+        throw new Error('Please provide a valid Email Address');
+    } else if (typeof phone === 'undefined' || !isNaN(phone)) {
+        throw new Error('Please provide a valid Phone Number');
+    }
 
-    console.log(rawFormData);
+    try {
 
-    // try {
-        
-    //     await sql`INSERT INTO customers (name, email, phone, status) VALUES (${fullname}, ${email}, ${phone}, ${status})`;
-        
-    //     console.log('A User has been created');
+        await sql`INSERT INTO customers (name, email, phone, status) VALUES (${fullname}, ${email}, ${phone}, 'Active')`;
 
-    //     redirect('/dashboard');
+    } catch (err) {
 
-    // } catch (err) {
-        
-    //     console.error('Database Error:', err);
-        
-    //     throw new Error('Failed to add the new Customer');
+        console.error('Database Error:', err);
 
-    // }
+        throw new Error('Failed to add the new Customer');
+
+    }
+
+    redirect('/customers');
 }
