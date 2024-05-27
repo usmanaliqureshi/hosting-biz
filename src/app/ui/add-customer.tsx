afd526e7b3223dbc5774dc6690fba0e159198fb9
Usman Ali Qureshi <usman@usmanaliqureshi.com>
2024-05-27 18:47:22 +0500

    Fixed form action execution

--- src/app/ui/add-customer.tsx
+++ src/app/ui/add-customer.tsx
@@ -43,7 +43,7 @@ export default async function AddCustomerForm() {
                       Cancel
                     </a>
                     <button
-                      type="button"
+                      type="submit"
                       className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
                     >
                       Save changes
